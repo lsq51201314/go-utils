3d78923f1af590ef9beff45d65adbd0a10e1c226
lsq51201314 <lsq51201314@126.com>
2023-12-15 17:37:39 +0800

    Initial commit

--- yaml.go
+++ yaml.go
@@ -2,8 +2,14 @@ package utils
 
 import "github.com/spf13/viper"
 
-func YamlLoad(config interface{}) (err error) {
-	viper.SetConfigFile("./config.yaml")
+func YamlLoad(config interface{}, file ...string) (err error) {
+	//配置文件
+	path := "./config.yaml"
+	if len(file) > 0 {
+		path = file[0]
+	}
+	//载入配置
+	viper.SetConfigFile(path)
 	if err = viper.ReadInConfig(); err != nil {
 		return
 	}
